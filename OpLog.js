10d289237471e5d8f1bf2239090fe6eb6741efcd
Giselle <gisalina0618@gmail.com>
2022-11-16 21:51:16 -0400

    Practica Operadores Logicos

---
+++ OpLog.js
@@ -0,0 +1,12 @@
+const aSmile = false;
+const bSmile = false;
+const alerta = "VERDADERO; ESTAMOS EN PROBLEMAS";
+
+if(aSmile == false && bSmile == false || aSmile == true && bSmile == true)
+{
+    console.log(alerta) 
+}
+else
+{
+    console.log("NO ESTAMOS EN PROBLEMAS");
+}
